830b2bc2b88163efb8ca11db18f0df909608eb1a
Ryfiel <ryfielvgonzales@gmail.com>
2024-12-05 18:21:54 +0800

    Removed the manually created .cs files that was already existing in models folder. Created the FOLDER STRUCTURE for the system also then created the needed files for each of the folders including the XAML files for the UI. Modified this system from Console Application to Windows Application which made creating WPF files and XAML Files possible.

--- SOTAM/Program.cs
+++ SOTAM/Program.cs
@@ -1,35 +1,36 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SOTAM;  // Ensure this matches your project's namespace
+using SOTAM.Models;  
 
 class Program
 {
     static void Main(string[] args)
     {
-        // Build a configuration object to load the appsettings.json file
+        
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json")
             .Build();
 
-        // Set up Dependency Injection (DI) for services
+        
         var serviceProvider = new ServiceCollection()
-            .AddDbContext<SOTAMContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("SOTAM"))) // Get connection string from appsettings
+            .AddDbContext<SotamContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("SOTAM"))) 
             .BuildServiceProvider();
 
-        // Get the DbContext instance from DI container
-        var context = serviceProvider.GetRequiredService<SOTAMContext>();
+        
+        var context = serviceProvider.GetRequiredService<SotamContext>();
 
-        // Now you can use the context to interact with the database
+        
         Console.WriteLine("Database connection is established!");
 
-        // Example of interacting with the database
-        var tables = context.Tables.ToList();  // Fetching tables from the database
+        
+        var tables = context.Tables.ToList();  
         foreach (var table in tables)
         {
-            Console.WriteLine($"Table {table.TableID} - {table.Status}");
+            Console.WriteLine($"Table {table.TableId} - {table.Status}");
         }
     }
 }
